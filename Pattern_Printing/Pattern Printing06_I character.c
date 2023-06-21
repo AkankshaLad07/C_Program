b2ab307b6182c6f2463f81b0bd781e0ee9288a23
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-06-21 19:46:40 +0530

    Pattern_Printing

---
+++ Pattern_Printing/Pattern Printing06_I character.c
@@ -0,0 +1,41 @@
+///program to display I for given size. 
+
+
+   #include<stdio.h>
+   #include<conio.h>
+   int main()
+   {
+       int i=0, j=0, r=0, c=0;
+       
+       printf("\nEnter row number:");
+       scanf("%d",&r);
+       
+       printf("Enter column number:");
+       scanf("%d",&c);
+       
+       printf("\n========= Pattern ========\n");
+       
+       for(i=1;i<=r;i++)
+       {
+          
+           for(j=1;j<=c;j++)
+           {
+             
+                if(i==1||i==r||j==(r/2)+1)
+                {
+                    printf(" * ");
+                }
+                 else
+                {
+                     printf("   ");
+                }
+              
+           }
+           
+           printf("\n");
+       }
+       printf("\n===========Thanks===========");
+       
+       getch();
+       return 0;
+   }
