69f48d6bcecba15b3a28bbb27f27b4fa5988fe13
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-06-21 19:56:51 +0530

    Pattern_printing

---
+++ Pattern_Printing/Pattern Printing08_Z character.c
@@ -0,0 +1,35 @@
+///program to display Z . 
+
+   #include<stdio.h>
+   #include<conio.h>
+   int main ()
+   {
+     int i=0, j=0, X=0;
+     
+     printf("Enter size for pattern :");
+     scanf("%d",&X);
+     
+     printf("\n=========== pattern ===========\n\n");
+     
+      for(i=1;i<=X;i++)
+      {
+         
+          for( j=1;j<=X;j++)
+          {
+            
+             if(i==1||i==X||i+j==X+1)
+             {
+                 printf(" * ");
+             }
+             else
+                 printf("   ");
+          }
+          
+          printf("\n");
+      }
+      
+      printf("\n==========Thanks==========");
+      
+      getch();
+      return 0;
+     }
---
+++ Pattern_Printing/Pattern Printing09_reverse Z.c
@@ -0,0 +1,32 @@
+///program  to display reverse Z character. 
+
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+  int i=0, j=0, X=0;
+  
+  printf("\nEnter size for pattern :");
+  scanf("%d",&X);
+  
+  printf("\n=========Pattern=======\n\n");
+  
+  for(i=1;i<=X;i++)
+  {
+    for( j=1;j<=X;j++)
+    {
+      if(i==1||i==X||i==j)
+      {
+        printf(" * ");
+      }
+      else
+      printf("   ");
+    }
+    printf("\n");
+  }
+  
+  printf("\n========Thanks========");
+  
+  getch();
+  return 0;
+}
