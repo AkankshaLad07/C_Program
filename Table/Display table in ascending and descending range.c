c35d2d78d206ebeae973b4de5626c3b3de15a3a2
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-07-26 23:07:32 +0530

    Table_Program

---
+++ Table/Display table in ascending and descending range.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+   int i=0,no=0,s=0, e=0;
+  
+   printf("\nTable starts from:");
+   scanf("%d",&s);
+   printf("\nTable upto:");
+   scanf("%d",&e);
+   printf("=================================\n"); 
+   if(s<e)
+   {
+       for(no=1;no<=10;no++) 
+        {
+            for(i=s;i<=e;i++)
+            {
+                printf(" %4d\t",no*i);     
+            } 
+          printf("\n");
+        }
+   }
+    else
+    {
+        for(no=1;no<=10;no++) 
+        {
+            for(i=s;i>=e;i--)
+            {
+                printf(" %4d\t",no*i);     
+            } 
+          printf("\n");
+        }
+    }
+  printf("==============================\n");
+  getch();
+  return 0;
+ }
