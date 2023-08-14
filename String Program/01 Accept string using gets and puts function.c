35100ddf5514e0b95794b3f030835f10312a5860
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-08-14 23:43:26 +0530

    String_program

---
+++ String Program/01 Accept string using gets and puts function.c
@@ -0,0 +1,22 @@
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+   char demo[15]={'K','A','R','A','D'};
+
+   puts(demo);
+
+   printf("\nEnter a string : ");
+   gets(demo);
+   printf("\nGiven string is : ");
+   puts(demo);
+
+
+    getch();
+    return 0;
+}
+
+
