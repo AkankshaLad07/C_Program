e1b55711bd09ee4223e015ae9bd41cf67e11d7e0
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-10-02 00:09:22 +0530

    Helper_Functions

---
+++ Case_Study/Helper_Functions/Function_Modulo.c
@@ -0,0 +1,14 @@
+int Mod()
+{
+    int Num1=0 , Num2=0, Ans=0;
+
+    printf("\nEnter 1st number: ");
+    scanf("%d",&Num1);
+    printf("\nEnter 2nd number: ");
+    scanf("%d",&Num2);
+
+    Ans=Num1%Num2;
+
+    printf("\n\n\t %d %% %d = %d",Num1,Num2,Ans);
+    return 0;
+}
