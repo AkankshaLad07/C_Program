f14c2e91f5e7990156e4a14279ebcac711ac2e86
AkankshaLad07 <127278599+AkankshaLad07@users.noreply.github.com>
2023-11-27 23:03:15 +0530

    Pointer

---
+++ Pointer/Pointer Implementation.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+    int iNo = 42,iNum = 37;
+    int *iPtr1 = &iNo;
+    int *iPtr2 = &iNo;
+
+
+    printf("\n Value Of iNo = %d",iNo);
+    printf("\n Address Of iNo =%d",&iNo);
+    printf("\n Value Of iNo Using Its Pointer iPtr = %d",*iPtr1);
+    printf("\n Address Of iNo Using iPtr = %d",iPtr1);
+    printf("\n Address Of iPtr = %d",&iPtr1);
+
+    printf("\n Value Of iNum = %d",iNum);
+    
+   getch();
+   return 0;
+ }
